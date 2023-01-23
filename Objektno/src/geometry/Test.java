7857684dea407c841d7a0ce53d5ea5b5db987bb1
Stefan <Stefan_Dusan_Ana@DESKTOP-VKCJDN3>
2023-01-23 17:34:32 +0100

    OOIT 23.01.

--- Objektno/src/geometry/Test.java
+++ Objektno/src/geometry/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
+
 		Circle c = new Circle(new Point(3,5), 7);
 		System.out.println(c.circumference());
 		System.out.println(c.area());
